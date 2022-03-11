49c0497634914bbea13ffdaca4c956b595946543
Arlyn <arlyn.goodrich@gmail.com>
2022-03-11 15:22:11 -0600

    DEV-2 Setup Plugin

---
+++ Plugins/CharacterLocomotion/Source/CharacterLocomotion/Private/CharacterLocomotion.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CharacterLocomotion.h"
+
+#define LOCTEXT_NAMESPACE "FCharacterLocomotionModule"
+
+void FCharacterLocomotionModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FCharacterLocomotionModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCharacterLocomotionModule, CharacterLocomotion)
