de0a64142a9ea9c72a65d57c78b8c771331b6a35
Arlyn <arlyn.goodrich@gmail.com>
2022-03-11 16:33:14 -0600

    DEV-3 Migrated Character Locomotion Code

--- Plugins/CharacterLocomotion/Source/CharacterLocomotion/Private/CustomCharacter.cpp
+++ Plugins/CharacterLocomotion/Source/CharacterLocomotion/Private/CustomCharacter.cpp
@@ -3,12 +3,49 @@
 
 #include "CustomCharacter.h"
 
-// Sets default values
-ACustomCharacter::ACustomCharacter()
-{
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+//UE4 Includes
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
 
+// Sets default values
+ACustomCharacter::ACustomCharacter(const FObjectInitializer& ObjectInitializer)
+
+
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<ACustomCharacter>(ACharacter::CharacterMovementComponentName))
+
+{
+
+	// ==== Default Values ===== =====
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	// ==== Components ===== =====
+
+	//Add Spring Arm for Camera
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->bUsePawnControlRotation = false;
+	SpringArmComp->bEnableCameraLag = true;
+	SpringArmComp->bEnableCameraRotationLag = true;
+	SpringArmComp->bInheritPitch = false;
+
+	//Spring Arm default values
+	SpringArmComp->SetRelativeLocation(FVector(20.f, 0.f, 85.f));
+	SpringArmComp->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f));
+	SpringArmComp->TargetArmLength = 150.f;
+
+	//Add Camera Component
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComp->SetupAttachment(SpringArmComp);
+
+
+	//Default Movement Values
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetCharacterMovement()->MaxWalkSpeed = DefaultWalkSpeed;
+	GetCharacterMovement()->MaxWalkSpeedCrouched = DefaultCrouchSpeed;
 }
 
 // Called when the game starts or when spawned
@@ -25,10 +62,167 @@ void ACustomCharacter::Tick(float DeltaTime)
 
 }
 
+/// ==== Replicated Variables ===== =====
+
+void ACustomCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty >& OutLifetimeProps) const
+{
+
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACustomCharacter, bWantsToSprint);
+	DOREPLIFETIME(ACustomCharacter, bIsSprinting);
+	DOREPLIFETIME(ACustomCharacter, SprintSpeedMuliplyer)
+
+
+}
+
+
+
+
+// ==== Movement ===== =====
+
+
 // Called to bind functionality to input
 void ACustomCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACustomCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACustomCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRight", this, &ACustomCharacter::TurnRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &ACustomCharacter::LookUp);
+
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACustomCharacter::Jump);
+	PlayerInputComponent->BindAction("ToggleCrouch", IE_Pressed, this, &ACustomCharacter::ToggleCrouch);
+	PlayerInputComponent->BindAction("ToggleSprint", IE_Pressed, this, &ACustomCharacter::SetWantsToSprint);
+
 }
 
+bool ACustomCharacter::GetWantsToSprint()
+{
+	// resets sprint if not moving
+	if (GetVelocity().Size() == 0 && bWantsToSprint == true)
+	{
+		SetWantsToSprint();
+	}
+
+	return bWantsToSprint;
+}
+
+float ACustomCharacter::GetSprintSpeedModifier() { return SprintSpeedMuliplyer; }
+
+void ACustomCharacter::SetSprintSpeedModifer(float NewSprintModifer)
+{
+	if (HasAuthority())
+	{
+		SprintSpeedMuliplyer = NewSprintModifer;
+	}
+
+}
+
+float ACustomCharacter::GetDefaultWalkSpeed() { return DefaultWalkSpeed; }
+
+
+//Forward and Back
+void ACustomCharacter::MoveForward(float Value)
+{
+
+	AddMovementInput(GetActorForwardVector() * Value);
+
+}
+
+
+//Right and Left
+void ACustomCharacter::MoveRight(float Value)
+{
+
+	AddMovementInput(GetActorRightVector() * Value);
+
+}
+
+void ACustomCharacter::TurnRight(float Value)
+{
+
+	AddControllerYawInput(Value);
+
+}
+
+
+void ACustomCharacter::LookUp(float Value)
+{
+
+	if (CameraComp == nullptr) { return; }
+
+	AddControllerPitchInput(Value);
+
+
+}
+
+void ACustomCharacter::Jump()
+{
+	Super::Jump();
+}
+
+void ACustomCharacter::ToggleCrouch()
+{
+	if (GetCharacterMovement()->IsCrouching())
+	{
+		UnCrouch();
+	}
+	else
+	{
+		Crouch();
+		if (bWantsToSprint == true) { SetWantsToSprint(); }
+	}
+}
+
+
+
+void ACustomCharacter::SetWantsToSprint()
+{
+
+	if (GetLocalRole() < ROLE_Authority)
+	{
+		// Is NOT Authority
+		Server_SetWantsToSprint();
+	}
+	else
+	{
+		//Is Authority
+		if (bWantsToSprint == false)
+		{
+			bWantsToSprint = true;
+			WantsToSprint_OnSprintStart.Broadcast();
+			//TODO Add Log FileUE_LOG(LogTPCharacter, Log, TEXT("Character wants to sprint"))
+
+		}
+		else
+		{
+			bWantsToSprint = false;
+			WantsToNotSprint_OnSptrintStop.Broadcast();
+			//TODO Add Log File UE_LOG(LogTPCharacter, Log, TEXT("Character wants to stop sprint"))
+		}
+	}
+}
+
+
+bool ACustomCharacter::Server_SetWantsToSprint_Validate()
+{
+	return true;
+}
+
+void ACustomCharacter::Server_SetWantsToSprint_Implementation()
+{
+	SetWantsToSprint();
+
+}
+
+float ACustomCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	Damage_DamageTaken.Broadcast(DamageAmount);
+
+	return DamageAmount;
+}
--- Plugins/CharacterLocomotion/Source/CharacterLocomotion/Public/CustomCharacter.h
+++ Plugins/CharacterLocomotion/Source/CharacterLocomotion/Public/CustomCharacter.h
@@ -6,6 +6,13 @@
 #include "GameFramework/Character.h"
 #include "CustomCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWantsToSprint);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWantsToNotSprint);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTakeDamageSignature, float, Damage);
+
 UCLASS()
 class CHARACTERLOCOMOTION_API ACustomCharacter : public ACharacter
 {
@@ -13,7 +20,7 @@ class CHARACTERLOCOMOTION_API ACustomCharacter : public ACharacter
 
 public:
 	// Sets default values for this character's properties
-	ACustomCharacter();
+	ACustomCharacter(const class FObjectInitializer& ObjectInitializer);
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,5 +32,80 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Called by Movement component to see if character wants to sprint
+	bool GetWantsToSprint();
+
+	// Called by Movement component to multiply default walk speed
+	float GetSprintSpeedModifier();
+
+	UFUNCTION()
+	void SetSprintSpeedModifer(float NewSprintModifer);
+
+	UFUNCTION()
+	void SetWantsToSprint();
+
+	float GetDefaultWalkSpeed();
+
+	UPROPERTY(BlueprintAssignable)
+	FWantsToSprint WantsToSprint_OnSprintStart;
+
+	UPROPERTY(BlueprintAssignable)
+	FWantsToNotSprint WantsToNotSprint_OnSptrintStop;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnTakeDamageSignature Damage_DamageTaken;
+
+protected:
+
+	// ==== Components ===== =====
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player Components")
+		UCameraComponent* CameraComp;
+
+
+	// ==== Movement Functions ===== =====
+
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void TurnRight(float Value);
+
+	void LookUp(float Value);
+
+	void Jump();
+
+	void ToggleCrouch();
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetWantsToSprint();
+	bool Server_SetWantsToSprint_Validate();
+	void Server_SetWantsToSprint_Implementation();
+
+	// ==== Movement Variables ===== =====
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Player Movement")
+		bool bWantsToSprint;
+
+	UPROPERTY(Replicated, BlueprintReadOnly, Category = "Player Movement")
+		bool bIsSprinting;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player Movement")
+		float DefaultWalkSpeed = 175;
+
+	UPROPERTY(Replicated, EditDefaultsOnly, Category = "Player Movement")
+		float SprintSpeedMuliplyer = 3;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Player Movement")
+		float DefaultCrouchSpeed = 150;
+
+	// ==== Damage ====
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser);
 
 };
