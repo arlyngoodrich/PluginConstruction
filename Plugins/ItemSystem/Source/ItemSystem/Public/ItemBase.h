40e5844a0e25a71a497ab756b2e19269ebb95954
Arlyn <arlyn.goodrich@gmail.com>
2022-03-12 15:39:22 -0600

    DEV-7 Added Item Base Class source file

---
+++ Plugins/ItemSystem/Source/ItemSystem/Public/ItemBase.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemData.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class ITEMSYSTEM_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	UPROPERTY(Replicated, EditAnywhere,BlueprintReadOnly,Category="Item Data")
+	FItemData ItemData;
+
+};
