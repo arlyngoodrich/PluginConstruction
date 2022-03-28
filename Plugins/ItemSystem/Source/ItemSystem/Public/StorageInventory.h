ccd41b0335badd6d53089ee6090a2594470c7d20
Arlyn <arlyn.goodrich@gmail.com>
2022-03-28 15:18:36 -0500

    DEV-27 Sub Classed Inventory into Player and Storage Inventory classes

---
+++ Plugins/ItemSystem/Source/ItemSystem/Public/StorageInventory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryComponent.h"
+#include "StorageInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITEMSYSTEM_API UStorageInventory : public UInventoryComponent
+{
+	GENERATED_BODY()
+	
+};
