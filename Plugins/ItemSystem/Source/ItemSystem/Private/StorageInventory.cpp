ac77cbefac64d924bbe1ab9e007ca30ce0fc4c09
Arlyn <arlyn.goodrich@gmail.com>
2022-03-31 11:02:53 -0500

    DEV-27 Open Close Inventory UI Framework

--- Plugins/ItemSystem/Source/ItemSystem/Private/StorageInventory.cpp
+++ Plugins/ItemSystem/Source/ItemSystem/Private/StorageInventory.cpp
@@ -3,7 +3,21 @@
 
 #include "StorageInventory.h"
 
-void UStorageInventory::OnInventoryInteract(APlayerController* InstigatingPlayer) const
+#include "ItemSystem.h"
+#include "GameFramework/PlayerController.h"
+#include "Net/UnrealNetwork.h"
+
+
+void UStorageInventory::GetLifetimeReplicatedProps(TArray<FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UStorageInventory, bIsInventoryOpen);
+}
+
+
+
+void UStorageInventory::OpenInventory(APlayerController* InstigatingPlayer)
 {
 	if(GetOwnerRole()!=ROLE_Authority)
 	{
@@ -13,6 +27,48 @@ void UStorageInventory::OnInventoryInteract(APlayerController* InstigatingPlayer
 	if(InstigatingPlayer != nullptr)
 	{
 		GetOwner()->SetOwner(Cast<AActor>(InstigatingPlayer));
+		bIsInventoryOpen = true;
+		
+		UE_LOG(LogItemSystem,Log,TEXT("%s set as owner of %s"),
+			*InstigatingPlayer->GetName(),*GetOwner()->GetName())
+
+		Client_AddTransferUI();
 	}
 	
+}
+
+void UStorageInventory::CloseInventory(APlayerController* InstigatingPlayer)
+{
+
+	if(GetOwnerRole()!=ROLE_Authority)
+	{
+		Server_CloseInventory(InstigatingPlayer);
+		return;
+	}
+
+	//Ensure same controller is closing inventory
+	if(GetOwner()->GetOwner() == InstigatingPlayer)
+	{
+		GetOwner()->SetOwner(nullptr);
+		bIsInventoryOpen = false;
+
+		UE_LOG(LogItemSystem,Log,TEXT("%s is not longer the owner of %s"),
+			*InstigatingPlayer->GetName(),*GetOwner()->GetName())
+		
+	}
+}
+
+bool UStorageInventory::Server_CloseInventory_Validate(APlayerController* InstigatingPlayer)
+{
+	return InstigatingPlayer == GetOwner()->GetOwner();
+}
+
+void UStorageInventory::Server_CloseInventory_Implementation(APlayerController* InstigatingPlayer)
+{
+	CloseInventory(InstigatingPlayer);
+}
+
+void UStorageInventory::Client_AddTransferUI_Implementation()
+{
+	AddTransferUI();
 }
--- Plugins/ItemSystem/Source/ItemSystem/Public/StorageInventory.h
+++ Plugins/ItemSystem/Source/ItemSystem/Public/StorageInventory.h
@@ -18,7 +18,29 @@ public:
 
 	//Validates player controller and set's owning actor ownership to the player controller.  This will facilitate RPC
 	//calls for the player.  
-	UFUNCTION(BlueprintAuthorityOnly,Category="Inventory")
-	void OnInventoryInteract(APlayerController* InstigatingPlayer) const;
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly,Category="Inventory")
+	void OpenInventory(APlayerController* InstigatingPlayer);
+
+	UFUNCTION(BlueprintCallable,Category="Inventory")
+	void CloseInventory(APlayerController* InstigatingPlayer);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Inventory")
+	void AddTransferUI();
+	
+
+
+protected:
+
+	UPROPERTY(Replicated,BlueprintReadOnly,Category="Inventory")
+	bool bIsInventoryOpen;
+
+	UFUNCTION(Server,Reliable,WithValidation)
+	void Server_CloseInventory(APlayerController* InstigatingPlayer);
+	bool Server_CloseInventory_Validate(APlayerController* InstigatingPlayer);
+	void Server_CloseInventory_Implementation(APlayerController* InstigatingPlayer);
+	
+	UFUNCTION(Client,Reliable)
+	void Client_AddTransferUI();
+	void Client_AddTransferUI_Implementation();
 	
 };
