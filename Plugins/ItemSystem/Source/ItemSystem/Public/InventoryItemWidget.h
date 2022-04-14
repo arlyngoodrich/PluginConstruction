7a0c2c8b03175ffb83c52916d1c0b8d5fe6c07b3
Arlyn <arlyn.goodrich@gmail.com>
2022-04-14 13:42:39 -0500

    DEV-78 Updated Item Widget to Live Doc

--- Plugins/ItemSystem/Source/ItemSystem/Public/InventoryItemWidget.h
+++ Plugins/ItemSystem/Source/ItemSystem/Public/InventoryItemWidget.h
@@ -11,8 +11,11 @@
 class UInventoryGridWidget;
 class UInventorySlotWidget;
 class UInventoryComponent;
+
+
+
 /**
- * 
+ * @brief Base User Widget used for Item Widgets.  Handles back end native coding. 
  */
 UCLASS()
 class ITEMSYSTEM_API UInventoryItemWidget : public UCustomUserWidget
@@ -20,20 +23,28 @@ class ITEMSYSTEM_API UInventoryItemWidget : public UCustomUserWidget
 	GENERATED_BODY()
 
 public:
-
-	//Inventory Item Data struct from the owning item.  Should be set before construct. 
+	
+	/**
+	 * @brief Inventory Item Data struct from the owning item.  Should be set before construct. 
+	 */
 	UPROPERTY(BlueprintReadOnly,Category="Inventory")
 	FInventoryItemData MyInventoryItemData;
-
-	//Owning Grid Widget.  Should be set before construct. 
+	
+	/**
+	 * @brief Owning Grid Widget.  Should be set before construct. 
+	 */
 	UPROPERTY(BlueprintReadOnly, Category="Inventory")
 	UInventoryGridWidget* OwningGridWidget;
-
-	//Owning Widget Slot used to set position.  Should be set before construct.  
+	
+	/**
+	 * @brief Owning Widget Slot used to set position.  Should be set before construct.  
+	 */
 	UPROPERTY(BlueprintReadOnly, Category="Inventory")
 	UInventorySlotWidget* OwningSlot;
-
-	//Owning Owning Inventory Component.  Should be set before construct.  
+	
+	/**
+	 * @brief Owning Owning Inventory Component.  Should be set before construct.  
+	 */
 	UPROPERTY(BlueprintReadOnly,Category="Inventory")
 	UInventoryComponent* OwningInventory;
 
@@ -43,16 +54,39 @@ public:
 	
 protected:
 	
+	/**
+	 * @brief Checks to see if item can move into position.
+	 * @param TargetPosition Position to move item to. 
+	 * @param bShouldRotate If the item should be rotated
+	 * @return returns true if Ok, false if not.
+	 */
 	UFUNCTION(BlueprintPure,Category="Inventory")
 	bool CanItemMoveToPosition(FInventory2D TargetPosition, bool bShouldRotate) const;
 
+	/**
+	 * @brief Checks if item can transfer to position
+	 * @param TargetInventory Inventory to transfer to
+	 * @param TargetPosition Position to transfer item to in target inventory
+	 * @param bShouldRotate If item should be rotated
+	 * @return returns true if Ok, false if not
+	 */
 	UFUNCTION(BlueprintPure,Category="Inventory")
 	bool CanItemTransferToPosition(UInventoryComponent* TargetInventory, FInventory2D TargetPosition,
 	                               bool bShouldRotate) const;
 
+	/**
+	 * @brief Move item target position
+	 * @param TargetPosition Position to move item to
+	 * @param bShouldRotate If item should be rotated
+	 */
 	UFUNCTION(BlueprintCallable,Category="Inventory")
 	void MoveItem(FInventory2D TargetPosition, bool bShouldRotate) const;
 	
+	/**
+	 * @brief Split item into new stack
+	 * @param TargetItemData Target item stack to split
+	 * @param NewStackQuantity Quantity for new item stack 
+	 */
 	UFUNCTION(BlueprintCallable,Category="Inventory")
 	void SplitItem(FInventoryItemData TargetItemData, int32 NewStackQuantity) const;
 };
