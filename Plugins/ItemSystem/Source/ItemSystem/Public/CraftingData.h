506ba0c64eb18912ac67380db60effd05f645f8b
Arlyn <arlyn.goodrich@gmail.com>
2022-04-14 21:07:04 -0500

    DEV-85 Added Crafting Component Class

--- Plugins/ItemSystem/Source/ItemSystem/Public/CraftingData.h
+++ Plugins/ItemSystem/Source/ItemSystem/Public/CraftingData.h
@@ -3,12 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "CraftingData.generated.h"
 
 class AItemBase;
 
 USTRUCT(BlueprintType)
-struct FRecipeComponent
+struct FRecipeComponent 
 {
 	GENERATED_USTRUCT_BODY()
 
@@ -23,5 +24,30 @@ struct FRecipeComponent
 	 */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Position")
 	int32 Quantity;
+
+};
+
+USTRUCT(BlueprintType)
+struct FCraftingRecipe : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	/**
+	 * @brief Name of Recipe
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Position")
+	FName RecipeName;
+
+	/**
+	 * @brief Array of items required to craft recipe 
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Position")
+	TArray<FRecipeComponent> RecipeInputs;
+
+	/**
+	 * @brief Array of items that are produced by the recipe 
+	 */	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Position")
+	TArray<FRecipeComponent> RecipeOutputs;
 
 };
