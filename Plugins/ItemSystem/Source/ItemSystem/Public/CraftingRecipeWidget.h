6c41afb4ee40875f1902e92180fd5051ea4b5600
Arlyn <arlyn.goodrich@gmail.com>
2022-04-21 10:04:20 -0500

    DEV-89 New Crafting Widget native classes

---
+++ Plugins/ItemSystem/Source/ItemSystem/Public/CraftingRecipeWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomUserWidget.h"
+#include "CraftingRecipeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITEMSYSTEM_API UCraftingRecipeWidget : public UCustomUserWidget
+{
+	GENERATED_BODY()
+	
+};
