5986de8ce5a90b17a23054f59d496794486d4fc7
Arlyn <arlyn.goodrich@gmail.com>
2022-04-21 09:57:11 -0500

    DEV-89 Added new Widgets

---
+++ Plugins/ItemSystem/Source/ItemSystem/Public/ItemManagementWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomUserWidget.h"
+#include "ItemManagementWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITEMSYSTEM_API UItemManagementWidget : public UCustomUserWidget
+{
+	GENERATED_BODY()
+	
+};
