5fa4d279c581cfd3433baa2bb0c2016a6099f543
Arlyn <arlyn.goodrich@gmail.com>
2022-04-28 10:07:53 -0500

    DEV-95 Created UI Interface Class

---
+++ Plugins/CustomWidgetTemplates/Source/CustomWidgetTemplates/Private/UIPlayerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UIPlayerInterface.h"
+
+
+// Add default functionality here for any IUIPlayerInterface functions that are not pure virtual.
