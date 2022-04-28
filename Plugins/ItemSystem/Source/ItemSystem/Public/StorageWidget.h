bebd17e2132b2a415599315edad09b34dddc5c66
Arlyn <arlyn.goodrich@gmail.com>
2022-04-28 10:08:25 -0500

    DEV-95 Creating Storage Widget

---
+++ Plugins/ItemSystem/Source/ItemSystem/Public/StorageWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CustomUserWidget.h"
+#include "StorageWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ITEMSYSTEM_API UStorageWidget : public UCustomUserWidget
+{
+	GENERATED_BODY()
+};
