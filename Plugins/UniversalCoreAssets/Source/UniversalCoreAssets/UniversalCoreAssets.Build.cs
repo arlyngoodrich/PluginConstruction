baa1fc664d1b52a5d2dceb5142882497226732a2
Arlyn <arlyn.goodrich@gmail.com>
2022-05-04 17:02:39 -0500

    DEV-153 added input core to UCA

--- Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/UniversalCoreAssets.Build.cs
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/UniversalCoreAssets.Build.cs
@@ -26,6 +26,7 @@ public class UniversalCoreAssets : ModuleRules
 			new string[]
 			{
 				"Core",
+				"InputCore"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
