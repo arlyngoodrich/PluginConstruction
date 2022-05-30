5af67e109c26411b0116653b30c15e8698e30ae4
Arlyn <arlyn.goodrich@gmail.com>
2022-05-30 14:46:09 -0500

    DEV-111 Added Custom Foliage ISMC source class

---
+++ Plugins/EnvironmentSystem/Source/EnvironmentSystem/Public/CustomFoliageISMC.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FoliageInstancedStaticMeshComponent.h"
+#include "CustomFoliageISMC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENVIRONMENTSYSTEM_API UCustomFoliageISMC : public UFoliageInstancedStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
