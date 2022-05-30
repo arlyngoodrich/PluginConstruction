233bca11e9bbddbc8a4168e0a5d90555a0bfca26
Arlyn <arlyn.goodrich@gmail.com>
2022-05-30 15:02:04 -0500

    DEV-111 Added Custom Foliage Base Actor

--- Plugins/EnvironmentSystem/Source/EnvironmentSystem/Public/FoliageSwapper.h
+++ Plugins/EnvironmentSystem/Source/EnvironmentSystem/Public/FoliageSwapper.h
@@ -24,5 +24,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+protected:
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category="Foliage System")
+	float SwapDistance;
+
+	
 		
 };
