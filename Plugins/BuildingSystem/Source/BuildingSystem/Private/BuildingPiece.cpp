5a27031a650ee2d58744138427639240a0d970e8
Arlyn <arlyn.goodrich@gmail.com>
2022-06-01 15:10:06 -0500

    DEV-103 Added Base Classes

---
+++ Plugins/BuildingSystem/Source/BuildingSystem/Private/BuildingPiece.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BuildingPiece.h"
+
+// Sets default values
+ABuildingPiece::ABuildingPiece()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABuildingPiece::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABuildingPiece::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
