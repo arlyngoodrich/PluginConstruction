576931ef81ce282f8f9428f344cf8ad3671628cf
Arlyn <arlyn.goodrich@gmail.com>
2022-06-07 15:18:59 -0500

    DEV-104 Added Building Snap Point Component

---
+++ Plugins/BuildingSystem/Source/BuildingSystem/Public/BuildingPieceSnapPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "BuildingPieceSnapPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGSYSTEM_API UBuildingPieceSnapPoint : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
