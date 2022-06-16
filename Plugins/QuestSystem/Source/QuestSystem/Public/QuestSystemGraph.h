8a3b8dd26a68d0e04dd0c50a09cc38c549344e6a
Arlyn <arlyn.goodrich@gmail.com>
2022-06-16 14:09:28 -0500

    DEV-181 Adding base Graph Classes

---
+++ Plugins/QuestSystem/Source/QuestSystem/Public/QuestSystemGraph.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericGraph.h"
+#include "QuestSystemGraph.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestSystemGraph : public UGenericGraph
+{
+	GENERATED_BODY()
+	
+};
