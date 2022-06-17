5dba4fdcaff4982b589c85abe7d988719238c42d
Arlyn <arlyn.goodrich@gmail.com>
2022-06-17 11:35:05 -0500

    DEV-181 Quest Task Base class

---
+++ Plugins/QuestSystem/Source/QuestSystem/Public/QuestTasks/QuestTaskBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "QuestTaskBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestTaskBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
