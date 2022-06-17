9c79dee6e06f2743aedd752cb82538699b67285c
Arlyn <arlyn.goodrich@gmail.com>
2022-06-16 21:02:27 -0500

    DEV-181 Added Base Class Nodes

---
+++ Plugins/QuestSystem/Source/QuestSystem/Public/QuestResolutionNode.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestSystemNode.h"
+#include "UObject/Object.h"
+#include "QuestResolutionNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestResolutionNode : public UQuestSystemNode
+{
+	GENERATED_BODY()
+};
---
+++ Plugins/QuestSystem/Source/QuestSystem/Public/QuestTaskNode.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "QuestSystemNode.h"
+#include "UObject/Object.h"
+#include "QuestTaskNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTSYSTEM_API UQuestTaskNode : public UQuestSystemNode
+{
+	GENERATED_BODY()
+};
