4b2a558e2222f0a530eac46af365580e11ac0d92
Arlyn <arlyn.goodrich@gmail.com>
2022-08-02 14:28:59 -0500

    DEV-131 Setup Base Health Attribute Set

--- Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Private/AbilitySystem/BaseHealthAttributeSet.cpp
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Private/AbilitySystem/BaseHealthAttributeSet.cpp
@@ -2,4 +2,21 @@
 
 
 #include "AbilitySystem/BaseHealthAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UBaseHealthAttributeSet::UBaseHealthAttributeSet()
+{
+	
+}
+
+void UBaseHealthAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UBaseHealthAttributeSet,Health);
+}
+
+void UBaseHealthAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseHealthAttributeSet,Health,OldValue);
+}
--- Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Public/AbilitySystem/BaseHealthAttributeSet.h
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Public/AbilitySystem/BaseHealthAttributeSet.h
@@ -4,7 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/BaseAttributeSet.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseHealthAttributeSet.generated.h"
+
+// Uses macros from Attributes.h
+#define ATTRIBUTE_ACCESSORS(ClassName,PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName,PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 
 /**
  * 
@@ -13,5 +23,18 @@ UCLASS()
 class UNIVERSALCOREASSETS_API UBaseHealthAttributeSet : public UBaseAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UBaseHealthAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health,Category="Attributes Health")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UBaseHealthAttributeSet,Health);
+
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldValue);
+	
 	
 };
