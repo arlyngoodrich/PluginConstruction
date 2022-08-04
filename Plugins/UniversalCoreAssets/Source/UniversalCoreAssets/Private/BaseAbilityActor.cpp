daf661acc3b98b605fb4df322fdbde1195c79bb3
Arlyn <arlyn.goodrich@gmail.com>
2022-08-04 12:26:51 -0500

    DEV-131 Created Base Ability Actor

---
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Private/BaseAbilityActor.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseAbilityActor.h"
+#include "AbilitySystem/BaseAbilitySystemComponent.h"
+
+
+// Sets default values
+ABaseAbilityActor::ABaseAbilityActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UBaseAbilitySystemComponent>(TEXT("Ability System Component"));
+}
+
+// Called when the game starts or when spawned
+void ABaseAbilityActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseAbilityActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+UAbilitySystemComponent* ABaseAbilityActor::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+
+
---
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Public/BaseAbilityActor.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Actor.h"
+#include "BaseAbilityActor.generated.h"
+
+class UBaseAbilitySystemComponent;
+UCLASS()
+class UNIVERSALCOREASSETS_API ABaseAbilityActor : public AActor , public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseAbilityActor();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components")
+	UBaseAbilitySystemComponent* AbilitySystemComponent;
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+};
