2560aa56cccff351c4bebff883cbd7bef596c945
Arlyn <arlyn.goodrich@gmail.com>
2022-08-09 10:43:04 -0500

    DEV-130 Implemented MVP Stamina Logic

--- Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Private/AbilitySystem/BaseStaminaAttributeSet.cpp
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Private/AbilitySystem/BaseStaminaAttributeSet.cpp
@@ -3,3 +3,38 @@
 
 #include "AbilitySystem/BaseStaminaAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UBaseStaminaAttributeSet::UBaseStaminaAttributeSet()
+{
+}
+
+void UBaseStaminaAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UBaseStaminaAttributeSet,Stamina);
+	DOREPLIFETIME(UBaseStaminaAttributeSet,MaxStamina);
+}
+
+void UBaseStaminaAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+}
+
+void UBaseStaminaAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+}
+
+void UBaseStaminaAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseStaminaAttributeSet,Stamina,OldValue);
+}
+
+void UBaseStaminaAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseStaminaAttributeSet,MaxStamina,OldValue);
+}
+
+
--- Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Public/AbilitySystem/BaseStaminaAttributeSet.h
+++ Plugins/UniversalCoreAssets/Source/UniversalCoreAssets/Public/AbilitySystem/BaseStaminaAttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystem/BaseAttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseStaminaAttributeSet.generated.h"
+
+// Uses macros from Attributes.h
+#define ATTRIBUTE_ACCESSORS(ClassName,PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName,PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,29 @@ UCLASS()
 class UNIVERSALCOREASSETS_API UBaseStaminaAttributeSet : public UBaseAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UBaseStaminaAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Stamina,Category="Attributes Stamina")
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UBaseStaminaAttributeSet,Stamina);
+
+	UFUNCTION()
+	virtual void OnRep_Stamina(const FGameplayAttributeData& OldValue);
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxStamina,Category="Attributes Stamina")
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UBaseStaminaAttributeSet,MaxStamina);
+
+	UFUNCTION()
+	virtual void OnRep_MaxStamina(const FGameplayAttributeData& OldValue);
 	
 };
